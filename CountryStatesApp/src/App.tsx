778f4f8963596501fb1ce59d96215cc9b6d5d4af
Sam Perry <samperry@Sams-MacBook-Air.local>
2024-03-11 16:33:23 -0400

    added static file handling for frontend

--- CountryStatesApp/src/App.tsx
+++ CountryStatesApp/src/App.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AddCountry from './components/AddCountry';
 import AddState from './components/AddState';

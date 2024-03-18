0b2d128a102bcc5c254ccd3c4559fc24e4e9f0cb
Sam Perry <samperry@Sams-MacBook-Air.local>
2024-03-18 13:23:42 -0400

    conditionally rendered the register button the header whether you are logged in or not

--- CountryStatesApp/src/components/Header.tsx
+++ CountryStatesApp/src/components/Header.tsx
@@ -10,8 +10,13 @@ const Header: React.FC = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ height: '70px' }}>
       <div className="container-fluid d-flex justify-content-between align-items-center">
         {/* Left-aligned Register*/}
+        {!isAuthenticated ? (
         <Link className="nav-link" to="/register">Register</Link>
-
+        ) : ( 
+        <div style={{ width: 'fit-content', visibility: 'hidden' }}>
+        <button className="nav-link" disabled>Register</button>
+        </div>
+        )}
         {/* Center-aligned title */}
         <Link className="navbar-brand mx-auto" to="/" style={{ fontSize: "30px", position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>Country and State App</Link>
 
